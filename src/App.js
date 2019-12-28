849e3acd49a45850687cd762e856d75be65029c9
Sabrina Becker <sbecka8@gmail.com>
2019-12-27 21:03:47 -0500

    Add fetch API to make GET requests to bookmarks API

--- src/App.js
+++ src/App.js
@@ -3,27 +3,55 @@ import './App.css';
 import AddBookmark from './AddBookmark';
 import BookmarkApp from './BookmarkApp';
 
-const bookmarks = [
-  {
-    title:"Google",  
-    url:"http://www.google.com",
-    rating:"3",
-    description:"No evil force"
-  },
-  {
-    title:"Tumblr",  
-    url:"http://www.tumblr.com",
-    rating:"5",
-    description:"May the force be with you"
-  }
-];
-
 class App extends Component {
+
+  constructor(props) {
+    super(props);
+    this.state = {
+      bookmarks: [],
+      showAddForm: false
+    };
+  };
+
+  componentDidMount() {
+    const url = 'https://tf-ed-bookmarks-api.herokuapp.com/v3/bookmarks';
+    const options = {
+      method: 'GET',
+      headers: {
+        "Authorization": "Bearer $2a$10$gXbWH/OZmwsSaPAq3R9QL..M9C3teLpvXlVkbrGpDUxQtkwwul1PO",
+        "Content-Type": "application/json"
+      }
+    };
+
+    fetch(url, options)
+      .then(response => {
+        if(!response.ok) {
+          throw new Error('Something went wrong, please try again later.');
+        }
+        return response;
+      })
+      .then(response => response.json())
+      .then(data => {
+        this.setState({
+          bookmarks: data,
+          error: null
+        });
+      })
+      .catch(err => {
+        this.setState({
+          error: err.message
+        });
+      });
+  };
+
   render() {
+    const page = this.state.showAddForm
+      ? <AddBookmark />
+      : <BookmarkApp bookmarks={this.state.bookmarks} />;
+
     return (
       <div className='App'>
-        <AddBookmark />
-        <BookmarkApp bookmarks={bookmarks}/>
+        {page}
       </div>
     );
   }
